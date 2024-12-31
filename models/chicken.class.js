e226557b94bc1ce70fb89f8b82ce0f0ae2d7d6ea
Maik <maik-joe@gmx.de>
2024-12-31 14:37:57 +0100

    clouds and bkg

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -3,6 +3,9 @@ class Chicken extends MoveableObject {
     
     constructor() {
         super().loadImage('img/3_enemies_chicken/chicken_small/1_walk/1_w.png');
+
+        this.x = 250 + Math.random() * 500;
     }
 
+   
 }
--- models/world.class.js
+++ models/world.class.js
@@ -5,6 +5,12 @@ class World {
     new Chicken(),
     new Chicken(),
 ];
+    clouds = [
+        new Cloud()
+    ];
+    backgroundObjects = [
+        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0,0)
+    ];
     ctx;
     canvas;
 
@@ -17,14 +23,27 @@ class World {
 
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
-        this.enemies.forEach(enemy => {
-            this.ctx.drawImage(enemy.img,enemy.x, enemy.y, enemy.width,enemy.height);
-        });
-
+        
+        this.addToMap(this.character);
+        this.addObjectsToMap(this.clouds);
+        this.addObjectsToMap(this.enemies);
+        this.addObjectsToMap(this.backgroundObjects);
+    
         let self = this;
         requestAnimationFrame(function () {
         self.draw();
     });
+    }
+
+
+    addObjectsToMap(objects) {
+        objects.forEach(o => {
+            this.addToMap(o);
+        });
+    }
+
+
+    addToMap(no) {
+        this.ctx.drawImage (no.img, no.x, no.y, no.width, no.height);
     }
 }
