658e8135f0ef6e69bd108230de341877a4a53f10
Maik <maik-joe@gmx.de>
2025-01-05 15:57:21 +0100

    movement Character L/R

--- models/character.class.js
+++ models/character.class.js
@@ -1,4 +1,4 @@
-class Character extends MoveableObject{
+class Character extends MoveableObject {
 
     Images_Walking = [
         'img/2_character_pepe/2_walk/W-21.png',
@@ -9,6 +9,9 @@ class Character extends MoveableObject{
         'img/2_character_pepe/2_walk/W-26.png'
     ];
 
+    world;
+    speed = 5;
+
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
         this.loadImages(this.Images_Walking);
@@ -17,16 +20,32 @@ class Character extends MoveableObject{
     }
 
     animate() {
+
         setInterval(() => {
-        let i = this.currentImage % this.Images_Walking.length;
-        let path = this.Images_Walking[i];
-        this.img = this.imageCache[path];
-        this.currentImage++;
-        },60); 
+            if (this.world.keyboard.RIGHT) {
+                this.x += this.speed;
+                this.otherDirection = false;
+            }
+
+            if (this.world.keyboard.LEFT) {
+                this.x -= this.speed;
+                this.otherDirection =true;
+            }
+        }, 1000 / 60);
+
+    setInterval(() => {
+
+        if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+            let i = this.currentImage % this.Images_Walking.length;
+            let path = this.Images_Walking[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+        }
+    }, 50);
     };
 
 
-    jump() {
+jump() {
 
-    };
+};
 }
--- models/world.class.js
+++ models/world.class.js
@@ -7,8 +7,8 @@ class World {
     ];
     clouds = [
         new Cloud('img/5_background/layers/4_clouds/1.png'),
-        new Cloud('img/5_background/layers/4_clouds/2.png'), 
-    ];
+        new Cloud('img/5_background/layers/4_clouds/2.png'),
+    ]; f
     backgroundObjects = [
         new BackgroundObject('img/5_background/layers/air.png', 0),
         new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0),
@@ -17,11 +17,18 @@ class World {
     ];
     ctx; // Canvas-Kontext
     canvas; // Canvas-Element
+    keyboard;
 
-    constructor(canvas) {
+    constructor(canvas, keyboard) {
         this.ctx = canvas.getContext("2d");
         this.canvas = canvas;
+        this.keyboard = keyboard;
         this.draw(); // Zeichen-Schleife starten
+        this.setWorld();
+    }
+
+    setWorld() {
+        this.character.world = this;
     }
 
     // Zeichnet die Welt
@@ -50,9 +57,18 @@ class World {
     }
 
     // Fügt ein einzelnes Objekt zur Karte hinzu
-    addToMap(no) {
-        if (no.img) { // Überprüfen, ob das Bild geladen ist
-            this.ctx.drawImage(no.img, no.x, no.y, no.width, no.height);
+    addToMap(mo) {
+        if (mo.otherDirection) {
+            this.ctx.save();
+            this.ctx.translate(mo.width, 0);
+            this.ctx.scale(-1, 1);
+            mo.x = mo.x * -1;
+        }
+        this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
+        if (mo.otherDirection) {
+            mo.x = mo.x * -1;
+            this.ctx.restore();
+          
         }
     }
 }
